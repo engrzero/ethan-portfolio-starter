b0362e3315e61791d0cfa7e89ff5579f737e6f84
Aldayr <engrzero@gmail.com>
2023-12-14 15:28:39 -0500

    Primer commit

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,13 @@
+// next image
+import Image from "next/image";
+
+// next Link 
+import Link from "next/link";
+
+// components
+import Socials from '../components/Socials'
 const Header = () => {
-  return <header>Header</header>;
+  return <header className='absolute z-30 w-full'>Header</header>;
 };
 
 export default Header;
